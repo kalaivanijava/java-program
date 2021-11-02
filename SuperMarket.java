e937b871046d609e4a43c11b15c3da330355b9bf
kalaivani <kalaielango@rediffmail.com>
2021-11-02 18:11:45 +0700

    Supermarket

---
+++ SuperMarket.java
@@ -0,0 +1,17 @@
+public class SuperMarket
+{
+static String name="KALAI SUPERMARKET";
+int mrp;
+public static void main(String[] args)
+{
+	name=" varsha supermarket";
+System.out.println(name);
+
+SuperMarket cake=new SuperMarket();
+cake.mrp=20;
+SuperMarket soap=new SuperMarket();
+soap.mrp=30;
+SuperMarket rice=new SuperMarket();
+rice.mrp=50;
+}
+}
