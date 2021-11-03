bad8d764b61e28bb1b3fe7b7bc38af7c2f35c41c
kalaivani <kalaielango@rediffmail.com>
2021-11-03 07:19:47 +0700

    Error

---
+++ Error.java
@@ -0,0 +1,11 @@
+public class Error
+{
+static int no=5;
+public static void main(String[] args)
+{
+Error e=new Error();
+System.out.println("Welcome to error program");
+System.out.println(no);
+System.out.println("hi");
+}
+}
