e483f85f60efb94f13e2393c06e40357aa4e2817
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-09 22:45:18 +0800

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,27 @@
+public class Customer
+{
+int amount=500;
+public static void main(String[] args)
+{
+Customer customer1=new Customer();
+int balance=customer1.purchase(200);
+System.out.println("main "+balance);
+customer1.pay(balance);
+System.out.println(customer1.amount);
+Customer customer2=new Customer();
+System.out.println(customer2.amount);
+}
+public void pay(int balance)
+{
+System.out.println("My balance"+balance);
+}
+
+
+
+public int purchase(int ip)
+{
+amount=amount-ip;
+System.out.println(amount);
+return amount;
+}
+}
