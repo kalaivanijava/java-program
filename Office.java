0b0745c1c0bc616c7098002c502b842ecd583c64
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-15 20:49:55 +0800

    Add files via upload

---
+++ Office.java
@@ -0,0 +1,20 @@
+package tamilnadu.chennai;
+public class Office
+{
+int ta=100;
+public static void main(String[] args)
+{
+Office employee1=new Office();
+System.out.println("Check");
+employee1.snacks();
+employee1.work();
+}
+void snacks()
+{
+System.out.println("Snacks");
+}
+public void work()
+{
+System.out.println("work");
+}
+}
