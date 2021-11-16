38e5077e8bfb3175543d7a06661bcf80da54a692
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-16 20:20:47 +0800

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,15 @@
+package nineties;
+public class Phone
+{
+public static void main(String[] args)
+{
+}
+public void call()
+{
+System.out.println(" calling");
+}
+public void receivedcalls()
+{
+System.out.println(" receiving calls");
+}
+}
