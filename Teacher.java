4e4832f8c9abd4a83c021a39148a05405cc072f3
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-18 18:19:47 +0800

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,17 @@
+public class Teacher
+{
+public static void main(String[] args)
+{
+Teacher t=new Teacher();
+t.teach();
+t.homework();
+}
+public void teach()
+{
+System.out.println(" teacher teaching");
+}
+public void homework()
+{
+System.out.println("Teacher giving homework ");
+}
+}
