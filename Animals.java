a56dec9ae2c3c4e9730f427d63a13453d40aeb16
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-18 17:59:45 +0800

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,18 @@
+//hierarchical inheritance
+public class Animals
+{
+public static void main(String[] args)
+{
+Animals a=new Animals();
+a.walk();
+a.run();
+}
+public void walk()
+{
+System.out.println(" Animals walk");
+}
+public void run()
+{
+System.out.println(" Animals run");
+}
+}
