ae2a1b7ddea1dfe0f8f618583626bfe476f0a119
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-20 21:16:18 +0800

    Add files via upload

---
+++ Round.java
@@ -0,0 +1,14 @@
+public class Round extends Shape
+{
+public static void main(String[] args)
+{
+Round r=new Round();
+r.draw();
+Shape s=new Shape();
+s.draw();
+}
+public void draw()
+{
+System.out.println(" draw round");
+}
+}
