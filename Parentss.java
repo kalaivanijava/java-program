344185fb7752a565e00b8bf0314cfa9e1ad1515f
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-22 17:51:30 +0800

    Add files via upload

---
+++ Parentss.java
@@ -0,0 +1,27 @@
+public class Parentss
+{
+	
+public Parentss()
+{
+System.out.println(" parents");
+}
+public Parentss(String name)
+{
+System.out.println(" parent one");
+}
+	/*public static void main(String[] args)
+	{
+		Parentss p=new Parentss();
+		Parentss a=new Parentss(kid);
+		p.msg();
+		a.call();*/
+
+	public void call()
+	{
+		System.out.println("parent calling");
+	}
+	public void msg()
+	{
+		System.out.println("parents msg");
+	}
+}
