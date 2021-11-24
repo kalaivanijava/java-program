00d5e902cc438533c6b24a249076a6b17971a877
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-24 20:43:43 +0800

    Add files via upload

---
+++ Watch.java
@@ -0,0 +1,15 @@
+public class Watch
+{
+int price=10;
+String brand="titan";
+public void buy()
+{
+System.out.println(" print price"+price);
+}
+public void check()
+{
+System.out.println(" Brand "+brand);
+}
+}
+
+
