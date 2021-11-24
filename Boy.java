6a690955aed220347cd6964c1bd81955be3e2d31
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2021-11-24 20:48:04 +0800

    Add files via upload

---
+++ Boy.java
@@ -0,0 +1,16 @@
+
+class Boy extends Humans
+{
+public static void main(String[] args)
+{
+Humans b=new Boy();
+Humans h=new Humans();
+h.walk();
+b.walk();
+}
+public static void walk()
+{
+System.out.println(" Boy walks");
+}
+}
+
