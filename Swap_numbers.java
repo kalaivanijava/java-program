31b00afb162adab2cb63ec5f732f71b6dd2382ec
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2022-01-08 08:33:47 +0700

    Add files via upload

---
+++ Swap_numbers.java
@@ -0,0 +1,13 @@
+package forloop.workings;
+
+public class Swap_numbers {
+public static void main(String[] args)
+{
+	int a=3,b=81;
+	b=a+b;
+   a=b-a;
+   b=b-a;
+   System.out.println(a);
+   System.out.println(b);
+}
+}
